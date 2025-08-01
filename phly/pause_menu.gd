337604c39eb5aedeee76aa6762f79b9892b756bc
bogdan <bogdan.g@planetgroupint.com>
2025-08-01 20:05:16 +0300

    added a pause menu

---
+++ phly/pause_menu.gd
@@ -0,0 +1,41 @@
+extends Control
+
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func testEsc():
+	if Input.is_action_just_pressed("Escape"):
+		print("pressed")
+		if get_tree().paused:
+			resume()
+		else:
+			pause()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	testEsc()
+
+
+func _on_resume_button_pressed() -> void:
+	resume()
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().reload_current_scene()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
