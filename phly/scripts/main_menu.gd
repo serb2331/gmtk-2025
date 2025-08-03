3f6814198f1696aa02f40698dfd30969360b9b04
bogdan <bogdan.g@planetgroupint.com>
2025-08-03 17:42:54 +0300

    partially done

--- phly/scripts/main_menu.gd
+++ phly/scripts/main_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var button_hover = $ButtonHoverSound
+@onready var button_click = $ButtonClickSound
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,10 +14,12 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
+	button_click.play()
 	GameState.start_game()
 
 
 func _on_exit_button_pressed() -> void:
+	button_click.play()
 	get_tree().quit()
 
 
@@ -26,3 +30,7 @@ func _on_options_button_pressed() -> void:
 
 func _on_credits_button_pressed() -> void:
 	pass # Replace with function body.
+
+
+func _on_mouse_entered() -> void:
+	button_hover.play()
